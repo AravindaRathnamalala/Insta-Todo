d8d48c54644aab3889d9ea4c4cf5ed77c14c3e86
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2024-03-03 22:42:59 +0800

    code refactoring.

--- app/src/main/java/com/araa/insta_todo/MainActivity.kt
+++ app/src/main/java/com/araa/insta_todo/MainActivity.kt
@@ -36,10 +36,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    InstaTodoTheme {
-//        Greeting("Android")
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InstaTodoTheme {
+        Greeting("Android")
+    }
+}
